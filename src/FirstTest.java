84e81ad4f2056c195555b9543de4ee25e6be0997
idunin <ilya.mirea@gmail.com>
2018-08-08 02:37:09 +0300

    Ex5: Test for saving two articles;

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -5,10 +5,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -32,6 +29,7 @@ public class FirstTest {
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "/home/dunin/Projects/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("commandTimeouts", 30);  // avoid infinite waiting, when POST hangs
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
@@ -445,6 +443,212 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testSaveTwoArticlesAndDelete()
+    {
+        String folderName = "ex5";
+
+        String searchTextToDelete = "Baseballs";
+        String articleTitleToDelete = "The Baseballs";
+        String searchTextToReopen = "Appium";
+        String articleTitleToReopen = "Appium";
+
+        findArticle(searchTextToDelete, articleTitleToDelete);
+        addToReadingList(folderName);
+        closeArticle();
+        findArticle(searchTextToReopen, articleTitleToReopen);
+        addToReadingList(folderName);
+        closeArticle();
+
+        openMyListsFolder(folderName);
+        deleteArticleFromFolder(articleTitleToDelete);
+
+        // Ensure that second article still in list
+        int articleCount = getAmountOfElements(By.xpath(String.format("//*[@text='%s']", articleTitleToReopen)));
+
+        Assert.assertTrue(
+                String.format("Article '%s' not found!", articleTitleToReopen),
+                articleCount == 1
+        );
+
+        reopenArticleFromList(articleTitleToReopen);
+    }
+
+    /**
+     * Reopen article form list and check title. Precondition: call openMyListsFolder
+     * @param articleTitle article to open
+     */
+    private void reopenArticleFromList(String articleTitle)
+    {
+        // Open article and check content
+        waitForElementAndClick(
+                By.xpath(String.format("//*[@text='%s']", articleTitle)),
+                "Cannot open article: " + articleTitle,
+                UI_INTERACTION_TIMEOUT);
+
+        String articleTitleText = waitForElementAndGetAttribute(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "text",
+                "Cannot find article title",
+                SERVER_INTERACTION_TIMEOUT);
+
+        Assert.assertEquals(
+                "Article title doesn't match, after open from saved lists",
+                articleTitle,
+                articleTitleText
+        );
+
+    }
+
+    /**
+     * Open My lists and click specified folder
+     * @param folderName folder name to click on
+     */
+    private void openMyListsFolder(String folderName)
+    {
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot open my lists page",
+                UI_INTERACTION_TIMEOUT
+        );
+
+        String folderLocator = String.format("//*[@resource-id='org.wikipedia:id/item_title'][@text='%s']", folderName);
+
+        // Added for reliability, because waitForElementAndClick completed successfully but no click performed
+        isElementPresentAndDisplayed(By.xpath(folderLocator));
+
+        waitForElementAndClick(
+                By.xpath(folderLocator),
+                "Cannot find folder: " + folderName,
+                UI_INTERACTION_TIMEOUT
+        );
+    }
+
+    /**
+     * Delete specified article. Preconditions: call openMyListsFolder
+     * @param articleText article to delete
+     */
+    private void deleteArticleFromFolder(String articleText)
+    {
+        swapElementToLeft(
+                By.xpath(String.format("//*[@text='%s']", articleText)),
+                "Cannot find saved article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath(String.format("//*[@text='%s']", articleText)),
+                "Cannot delete article: " + articleText,
+                UI_INTERACTION_TIMEOUT
+        );
+    }
+
+    /**
+     * Open More Options -> Add to reading list
+     * If called for the first time, click on Got IT
+     * Note: will fail, when trying to add article into not existing folder!
+     * @param folderName folder to add article (folder should already exist!)
+     */
+    private void addToReadingList(String folderName)
+    {
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find More options button!",
+                UI_INTERACTION_TIMEOUT
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.TextView[@text='Add to reading list']"),
+                "Cannot add article to reading list, item not found",
+                UI_INTERACTION_TIMEOUT
+        );
+
+        // Check if we already create reading list(s)
+        if (isElementPresentAndDisplayed(By.id("org.wikipedia:id/onboarding_button")))
+        {
+            waitForElementAndClick(
+                    By.id("org.wikipedia:id/onboarding_button"),
+                    "Cannot click on 'Got It' button!",
+                    UI_INTERACTION_TIMEOUT
+            );
+
+            waitForElementAndClear(
+                    By.id("org.wikipedia:id/text_input"),
+                    "Cannot find 'Name of list' input element!",
+                    UI_INTERACTION_TIMEOUT
+            );
+
+            waitForElementAndSendKeys(
+                    By.id("org.wikipedia:id/text_input"),
+                    folderName,
+                    "Cannot find 'Name of list' input, cannot input text",
+                    UI_INTERACTION_TIMEOUT
+            );
+
+            waitForElementAndClick(
+                    By.xpath("//*[@text='OK']"),
+                    "Cannot click 'Ok' button!",
+                    UI_INTERACTION_TIMEOUT
+            );
+        }
+        else
+        {
+            waitForElementAndClick(
+                    By.xpath(String.format("//*[@resource-id='org.wikipedia:id/item_title'][@text='%s']", folderName)),
+                    String.format("Cannot find folder '%s' in reading list", folderName),
+                    UI_INTERACTION_TIMEOUT
+            );
+        }
+    }
+
+    /**
+     * Find and open article
+     * @param searchText search text
+     * @param articleText exact article title
+     */
+    private void findArticle(String searchText, String articleText)
+    {
+        String articleLocatorJava = String.format("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='%s']", articleText);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find search input on main page",
+                UI_INTERACTION_TIMEOUT
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                searchText,
+                "Cannot find search input!",
+                UI_INTERACTION_TIMEOUT
+        );
+
+        waitForElementAndClick(
+                By.xpath(articleLocatorJava),
+                "Cannot find article: '" + articleText + "'!",
+                SERVER_INTERACTION_TIMEOUT);
+
+        waitElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title: " + articleText,
+                SERVER_INTERACTION_TIMEOUT
+        );
+    }
+
+    /**
+     * Close open article. Preconditions: call findArticle
+     */
+    private void closeArticle()
+    {
+        // For reliability
+        isElementPresentAndDisplayed(By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"));
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article (button X not found!)",
+                UI_INTERACTION_TIMEOUT
+        );
+    }
+
 
     private WebElement waitElementPresent(By by, String errorMessage)
     {
@@ -562,7 +766,7 @@ public class FirstTest {
         TouchAction action = new TouchAction(driver);
         action
                 .press(right_x, middle_y)
-                .waitAction(150)
+                .waitAction(300)
                 .moveTo(left_x, middle_y)
                 .release()
                 .perform();
@@ -572,6 +776,22 @@ public class FirstTest {
     {
         List elements = driver.findElements(by);
         return elements.size();
+    }
+
+    /**
+     * Check if element present on page
+     * @param by locator
+     * @return True if element present, else False
+     */
+    private boolean isElementPresentAndDisplayed(By by)
+    {
+        try {
+            WebElement element = waitElementPresent(by, "");
+            return element.isDisplayed();
+        } catch (TimeoutException e)
+        {
+            return false;
+        }
     }
 
     private void assertElementNotPresent(By by, String error_message)
