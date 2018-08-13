0551b81a2983e181e32a9a792921bbdaa3512cc0
idunin <ilya.mirea@gmail.com>
2018-08-14 00:18:51 +0300

    Refactoring: Add CoreTestCase;

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,47 +1,16 @@
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
 import java.util.List;
 
-public class FirstTest {
-    private AppiumDriver driver;
-    private int SERVER_INTERACTION_TIMEOUT = 15;
-    private int UI_INTERACTION_TIMEOUT = 5;
-
-    @Before
-    public void setUp() throws Exception
-    {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/home/dunin/Projects/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        capabilities.setCapability("commandTimeouts", 30);  // avoid infinite waiting, when POST hangs
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
-
-    @After
-    public void tearDown()
-    {
-        driver.quit();
-    }
-
+public class FirstTest extends CoreTestCase {
     @Test
-    public void firstTest()
+    public void testSearch()
     {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
@@ -286,7 +255,7 @@ public class FirstTest {
     }
 
     @Test
-    public void saveFirstArticleToMyList()
+    public void testSaveFirstArticleToMyList()
     {
         String folderName = "Learning programming";
         String searchText = "Java";
---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,41 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCase extends TestCase {
+    private static String appiumURL = "http://127.0.0.1:4723/wd/hub";
+    protected AppiumDriver driver;
+    protected int SERVER_INTERACTION_TIMEOUT = 15;
+    protected int UI_INTERACTION_TIMEOUT = 5;
+
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/home/dunin/Projects/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("commandTimeouts", 30);  // avoid infinite waiting, when POST hangs
+
+        driver = new AndroidDriver(new URL(appiumURL), capabilities);
+    }
+
+    @Override
+    public void tearDown() throws Exception
+    {
+        driver.quit();
+
+        super.tearDown();
+    }
+}
